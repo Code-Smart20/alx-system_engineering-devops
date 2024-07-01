23cf127d66b7f455abb73cfdd384d4891d4199ed
root <you@example.com>
2024-07-01 03:32:57 -0700

    puppet lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # creating a file in /tmp using puppet
 
 file {'achiever_coder':
-  path   => '/tmp/school',
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'I love Puppet',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
